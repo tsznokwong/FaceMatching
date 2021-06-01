9604c64d40d429387ac59ae73b6349c2e3fc3c16
tnwong <wong.tsznok@gmail.com>
2021-06-01 23:00:56 +0800

    feat: registering

--- view_controller.py
+++ view_controller.py
@@ -1,12 +1,15 @@
 from tkinter import *
+import tkinter.ttk as ttk
 import cv2
 from PIL import Image, ImageTk
 from authentication import *
+from threading import Timer
 
 class ViewController():
 
   def __init__(self):
     self.auth = Authenticator()
+    self.batch_size = 128
     self.window = Tk()
 
     self.window.title("Face ID Application")
@@ -16,24 +19,69 @@ class ViewController():
     self.panel = Label(self.window)
     self.panel.pack(padx=8, pady=8)
 
-    self.register_button = Button(self.window, text="Login", command=self.on_click_register)
+    self.register_button = Button(self.window, text="Register", command=self.on_click_register)
     self.register_button.pack(padx=8, pady=8)
+
+    self.counter = None
+    self.count_down_label = Label(self.window, text="0")
+    self.count_down_label.pack(padx=8, pady=8)
+
+    self.recording = False
+    self.recording_progress = ttk.Progressbar(self.window, maximum=self.batch_size)
+    self.recording_progress.pack(padx=8, pady=8)
+    self.recording_frames = []
 
     self.camera = cv2.VideoCapture(0)
     self.update_ui()
 
   def update_ui(self):
-    text = "Logout" if self.auth.isLoggedIn() else "Register"
+    self.update_register_button()
+    self.update_counter()
+    self.update_progress()
+    
+  def update_register_button(self):
+    text = f"Logout {self.auth.get_user_id()}" if self.auth.isLoggedIn() else "Register"
     self.register_button.text = text
     self.register_button.configure(text=text)
 
+  def update_counter(self):
+    if self.counter == None:
+      self.count_down_label.pack_forget()
+    else:
+      text = str(self.counter)
+      self.count_down_label.text = text
+      self.count_down_label.configure(text=text)
+      self.count_down_label.pack()
+
+  def update_progress(self):
+    if self.recording:
+      self.recording_progress.pack()
+    else:
+      self.recording_progress.pack_forget()
+    
+  def count_down(self):
+    self.counter -= 1
+    if self.counter == 0:
+      self.counter = None
+      self.update_counter()
+      self.recording = True
+      self.recording_progress.value = 0
+      self.recording_progress.configure(value=0)
+      self.update_progress()
+      return
+    self.update_counter()
+    t = Timer(1, self.count_down)
+    t.start()
    
   def on_click_register(self):
     if self.auth.isLoggedIn():
       self.auth.logout()
+      self.update_register_button()
     else:
-      self.auth.login("abc")
-    self.update_ui()
+      self.register_button.pack_forget()
+      self.counter = 4
+      self.count_down()
+      self.update_counter()
 
   def video_loop(self):
     success, frame = self.camera.read()
@@ -43,10 +91,16 @@ class ViewController():
       image = crop_square(image)
       image = image.transpose(Image.FLIP_LEFT_RIGHT)
       imgtk = ImageTk.PhotoImage(image=image)
+      if self.recording:
+        self.recording_frames.append(image)
+        self.recording_progress.step()
+        if (self.batch_size == len(self.recording_frames)):
+          self.recording = False
+          self.update_progress()
+          print(type(image))
       self.panel.imgtk = imgtk
       self.panel.configure(image=imgtk)
-      cv2.waitKey(100)
-      self.window.after(100, self.video_loop)
+      self.window.after(50, self.video_loop)
 
   def start(self):
     self.video_loop()
